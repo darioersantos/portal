56925c7d197353809d7597812f463720a00e2cc4
darioersantos <darioersantos@gmail.com>
2026-07-15 19:13:46 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service Worker - Portal Sport Zone Vasco da Gama */
-var CACHE='spz-portal-v163';
+var CACHE='spz-portal-v164';
 var ASSETS=['./','./index.html','./cofre.html','./lembretes.html','./aniversario.html','./limpeza.html','./config.js','./links.js','./manifest.json'];
 self.addEventListener('install',function(e){
   e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS);}).then(function(){return self.skipWaiting();}));
